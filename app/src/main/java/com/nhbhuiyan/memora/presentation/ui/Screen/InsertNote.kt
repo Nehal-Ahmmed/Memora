7fa9aff1810bf5c54a29f2ba192fc1e4ad215bd8
nehal <nhbhuiyan2003@gmail.com>
2025-05-24 17:36:17 +0600

    update demo

--- app/src/main/java/com/nhbhuiyan/memora/presentation/ui/Screen/InsertNote.kt
+++ app/src/main/java/com/nhbhuiyan/memora/presentation/ui/Screen/InsertNote.kt
@@ -35,7 +35,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.nhbhuiyan.memora.presentation.ui.theme.black
